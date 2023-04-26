a96096fe8cbfec2928b17bcfb04d9c45bee10be7
GridWhiz <gridwhiz@GridWhizs-MacBook-Pro-2.local>
2023-04-26 10:50:38 +0700

    path web hook

--- app/layers/deliveries/http/omise/handler.go
+++ app/layers/deliveries/http/omise/handler.go
@@ -17,6 +17,7 @@ func NewEndpointHttpHandler(ginEngine *gin.Engine,
 
 	v1 := ginEngine.Group("v1")
 	{
-		v1.POST("/omise/web/hooks", handler.WebHooks)
+		// v1.POST("/omise/web/hooks", handler.WebHooks)
+		v1.POST("/hooks", handler.WebHooks)
 	}
 }
