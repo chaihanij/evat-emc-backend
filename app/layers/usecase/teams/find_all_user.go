21ac30eb17a16f21835b318ed998b495eae7cb62
sancahi <sanchaipangboot@gmail.com>
2023-05-17 10:59:13 +0700

    edit condition search team

--- app/layers/usecase/teams/find_all_user.go
+++ app/layers/usecase/teams/find_all_user.go
@@ -27,7 +27,7 @@ func (u useCase) findAllTeam(wg *sync.WaitGroup, ctx context.Context, input *ent
 	result, err := u.TeamsRepo.FindAllTeam(ctx, input)
 
 	// if input.Name != nil {
-	if *input.Name == "" {
+	if *input.Name != "" {
 		resultmember, _ := u.UsersRepo.FindAllUser(ctx, &entities.UserFilter{Tel: input.Name})
 		if resultmember != nil {
 			for _, v := range resultmember {
