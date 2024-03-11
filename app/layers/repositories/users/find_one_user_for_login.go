8d31019a253709613c9d509b259409969d0c477c
Teeraphat Sirirak <teeraphat@gridwhiz.com>
2024-03-11 16:22:57 +0700

    filter year 2

--- app/layers/repositories/users/find_one_user_for_login.go
+++ app/layers/repositories/users/find_one_user_for_login.go
@@ -35,7 +35,7 @@ func (r repo) FindOneUserLogin(ctx context.Context, input *entities.UserFilter) 
 	///OAK test 2024 login
 	if user.Year != "2024"{
 		log.Print("Account is not in 2024:",user.Year)
-		return nil, errors.RecordNotFoundError{Message: fmt.Sprintf("Account is not in 2024")}
+		return nil, errors.RecordNotFoundError{Message: fmt.Sprintf("Account is not in 2024:",user.Year)}
 	}
 	
 	if err != nil{
