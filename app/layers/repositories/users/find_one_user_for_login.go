9c5db8a57519714233cc094a7cd3d00d0af84fc0
Teeraphat Sirirak <teeraphat@gridwhiz.com>
2024-03-11 16:19:35 +0700

    filter year

--- app/layers/repositories/users/find_one_user_for_login.go
+++ app/layers/repositories/users/find_one_user_for_login.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 
 	log "github.com/sirupsen/logrus"
-	//"gitlab.com/chaihanij/evat/app/errors"
+	"gitlab.com/chaihanij/evat/app/errors"
 	"gitlab.com/chaihanij/evat/app/constants"
 	"gitlab.com/chaihanij/evat/app/entities"
 	"gitlab.com/chaihanij/evat/app/env"
@@ -33,10 +33,10 @@ func (r repo) FindOneUserLogin(ctx context.Context, input *entities.UserFilter) 
 		Decode(&user)
 
 	///OAK test 2024 login
-	// if user.Year != "2024"{
-	// 	log.Print("Account is not in 2024:",user.Year)
-	// 	return nil, errors.RecordNotFoundError{Message: fmt.Sprintf("Account is not in 2024")}
-	// }
+	if user.Year != "2024"{
+		log.Print("Account is not in 2024:",user.Year)
+		return nil, errors.RecordNotFoundError{Message: fmt.Sprintf("Account is not in 2024")}
+	}
 	
 	if err != nil{
 		log.WithError(err).Errorln("DB FindOneUser Error")
